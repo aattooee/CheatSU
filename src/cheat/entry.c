1397149d4d1e7938fb2f6042f43094512ad40c7c
aattooee <2722864369@qq.com>
2025-02-15 13:41:23 +0800

    added operation result code enum

--- src/cheat/entry.c
+++ src/cheat/entry.c
@@ -6,8 +6,6 @@
 #include "memory.h"
 #include "process.h"
 
-
-
 int cheat_open(struct inode *node, struct file *file)
 {
 	return 0;
@@ -18,64 +16,51 @@ int cheat_close(struct inode *node, struct file *file)
 	return 0;
 }
 
-long cheat_ioctl(struct file *const file, unsigned int const cmd, unsigned long const arg)
+long cheat_ioctl(struct file *const file, unsigned int const cmd,
+		 unsigned long const arg)
 {
 	static COPY_MEMORY cm;
 	static MODULE_BASE mb;
-	static char key[0x100] = {0};
-	static char name[0x100] = {0};
-	static bool is_verified = false;
+	static char name[0x100] = { 0 };
 
-	if (cmd == OP_INIT_KEY && !is_verified)
-	{
-		if (copy_from_user(key, (void __user *)arg, sizeof(key) - 1) != 0)
-		{
-			return -1;
+	switch (cmd) {
+	case OP_READ_MEM: {
+		if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0) {
+			return ERROR_TOUCH_ARGS_FAILED;
 		}
-	}
-	switch (cmd)
-	{
-	case OP_READ_MEM:
-	{
-		if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0)
-		{
-			return -1;
-		}
-		if (read_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false)
-		{
-			return -1;
+		if (read_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) ==
+		    false) {
+			return ERROR_READ_MEM_FAILED;
 		}
 		break;
 	}
-	case OP_WRITE_MEM:
-	{
-		if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0)
-		{
-			return -1;
+	case OP_WRITE_MEM: {
+		if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0) {
+			return ERROR_TOUCH_ARGS_FAILED;
 		}
-		if (write_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false)
-		{
-			return -1;
+		if (write_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) ==
+		    false) {
+			return ERROR_WRITE_MEM_FAILED;
 		}
 		break;
 	}
-	case OP_MODULE_BASE:
-	{
-		if (copy_from_user(&mb, (void __user *)arg, sizeof(mb)) != 0 || copy_from_user(name, (void __user *)mb.name, sizeof(name) - 1) != 0)
-		{
-			return -1;
+	case OP_MODULE_BASE: {
+		if (copy_from_user(&mb, (void __user *)arg, sizeof(mb)) != 0 ||
+		    copy_from_user(name, (void __user *)mb.name,
+				   sizeof(name) - 1) != 0) {
+			return ERROR_TOUCH_ARGS_FAILED;
 		}
 		mb.base = get_module_base(mb.pid, name);
-		if (copy_to_user((void __user *)arg, &mb, sizeof(mb)) != 0)
-		{
-			return -1;
+		if (copy_to_user((void __user *)arg, &mb, sizeof(mb)) != 0) {
+			return ERROR_GET_MODULE_BASE_FAILED;
 		}
 		break;
 	}
 	default:
-		break;
+		return ERROR_UNKNOWN_OP;
 	}
-	return 0;
+
+	return OP_SUCCESS;
 }
 
 struct file_operations cheat_fops = {
@@ -85,9 +70,8 @@ struct file_operations cheat_fops = {
 	.unlocked_ioctl = cheat_ioctl,
 };
 
-int get_cheat_tool_handle(void){
-
-	int fd=anon_inode_getfd("/data/adb/ksud", &cheat_fops, NULL, O_RDWR);
+int get_cheat_tool_handle(void)
+{
+	int fd = anon_inode_getfd("/data/adb/ksud", &cheat_fops, NULL, O_RDWR);
 	return fd;
-
 }
